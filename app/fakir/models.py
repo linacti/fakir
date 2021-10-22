047f74be49ccdaae71657c7a19f3ece26cf40fd0
KamilTerelak1 <19dg.kamil.terelak@ezse.pl>
2021-10-22 15:17:40 +0200

    sprzedawca

--- app/fakir/models.py
+++ app/fakir/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Firma(models.Model):
+    nazwa_firmy = models.CharField(max_length=100)
+    adres_firmy = models.CharField(max_lenght=100)
+    podatek = models.CharField(max_length=20)
+    data_zalozenia = models.DateField('Data założenia')
+    Telefon_firmy = models.TextField()
+    NIP = models..TextField() 
+    
+ 
+   
+    def __str__(self):
+        return self.nazwa
