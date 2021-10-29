5b286747fe10b3efd95aaaf448ff143e0bfb36fb
dawideeq <19dg.dawid.pabis@ezse.pl>
2021-10-29 11:08:53 +0200

    Admin PozycjiFaktury

--- app/fakir/admin.py
+++ app/fakir/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Klient, Sprzedawca, Faktura, PozycjaFaktury, 
 
-# Register your models here.
+
+class PozycjaFakturyAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'faktura', 'podatek',)
+
+admin.site.register(Faktura, FakturaAdmin)
+admin.site.register(Sprzedawca, SprzedawcaAdmin)
+admin.site.register(PozycjaFaktury, PozycjaFakturyAdmin)
