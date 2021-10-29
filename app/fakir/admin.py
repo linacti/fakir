fbf999d3feeb3927219b5c8436fff1a2e604df50
KamilTerelak1 <19dg.kamil.terelak@ezse.pl>
2021-10-29 11:10:52 +0200

    Admin Sprzedawca

--- app/fakir/admin.py
+++ app/fakir/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Klient, Sprzedawca, Faktura, PozycjaFaktury, 
 
+class PozycjeInline(admin.TabularInline):
+    model = PozycjaFaktury
+    extra = 3
 
+
+class SprzedawcaAdmin(admin.ModelAdmin):
+    list_display = ('Sprzedawca_nazwa', 'Sprzedawca_podatek',)
+    search_fields = ("Sprzedawca_nazwa", "Sprzedawca_podatek")
+    
 class PozycjaFakturyAdmin(admin.ModelAdmin):
     list_display = ('nazwa', 'faktura', 'podatek',)
 
