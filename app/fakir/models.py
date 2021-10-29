665e0ad021d9cfe1c379f317045555c91ae316d4
KamilTerelak1 <19dg.kamil.terelak@ezse.pl>
2021-10-29 14:35:00 +0200

    poprawa

--- app/fakir/models.py
+++ app/fakir/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.db.models.fields import DecimalField
+from django.core.validators import RegexValidator
 
 class Klient(models.Model):
     Imie = models.CharField(max_length=100)
     Nazwisko = models.CharField(max_length=100)
-    nr_pesel = models.TextField(max_length=11)
+    pesel = RegexValidator(regex=r'^\+?1?\d{11,11}$', message="pesel musi miec 11 liter")
+    nr_pesel = models.CharField(validators=[pesel], max_length=11, blank=True) 
     Adres = models.CharField(max_length=150)
-    kod_pocztowy = models.TextField(max_length=6)
+    kod_pocztowy = models.CharField(max_length=6)
+    Adres = models.CharField(max_length=150)
     miasto = models.CharField(max_length=50)
     data_urodzenia = models.DateField()
-    nr_telefonu = models.TextField(max_length=9)
-    
+    nr_telefonu = models.IntegerField()
     
 
 class Sprzedawca(models.Model):
@@ -19,22 +21,23 @@ class Sprzedawca(models.Model):
     
     Sprzedawca_adres = models.CharField(max_length=100)
     
-    Sprzedawca_podatek = models.CharField(max_length=20)
+    Sprzedawca_podatek = models.IntegerField()
     
     Sprzedawca_data_zalozenia = models.DateField('Data założenia')
     
-    Sprzedawca_telefon = models.TextField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    Sprzedawca_numer = models.CharField(validators=[phone_regex], max_length=17, blank=True) 
     
-    Sprzedawca_NIP = models.TextField() 
     
-    Sprzedawca_wlasciciel = models.TextField()
-    
+    nip = RegexValidator(regex=r'^\+?1?\d{10,10}$', message="Nip must have 10 digits")
+    Sprzedawca_NIP = models.CharField(validators=[nip], max_length=10, blank=True)
+
     class Meta:
         verbose_name = "Firma"
         
         verbose_name_plural = "Sprzedawcy"
     def __str__(self):
-        return self.nazwa
+        return self.Sprzedawca_nazwa
         
 class Faktura(models.Model):
 
